d086fd4bf5bf5758ae8e6854327467f8d23d24e1
Christopher White <ckwhite@ieee.org>
2018-11-18 01:29:42 -0600

    Converted to DataFrames

--- hwk3/white_exercise03/neisaitou.py
+++ hwk3/white_exercise03/neisaitou.py
@@ -5,6 +5,7 @@
 ###
 
 import numpy as np
+import pandas as pd
 
 class Sequence(object):
     """
@@ -13,8 +14,8 @@ class Sequence(object):
 
     def __init__(self, seqID, strSeq):
         self._seq = strSeq
-        self._id = seqID
-        self._edge = None
+        self._id = str(seqID)
+        self.edge = None
 
     @property
     def name(self):
@@ -40,8 +41,17 @@ class Node(object):
     Class for the internal nodes of the tree
     """
 
-    def __init__(self):
+    def __init__(self, name=None):
         self._edges = []
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
 
     @property
     def edges(self):
@@ -63,13 +73,26 @@ class Edge(object):
     Class for the edges of the tree
     """
 
-    def __init__(self):
-        self._nodes = []
-        self._length = None
+    def __init__(self, head, tail, length=None):
+        self.head = head
+        self.tail = tail
+        self.length = length
 
     @property
-    def nodes(self):
-        return self._nodes
+    def head(self):
+        return self._head
+
+    @head.setter
+    def head(self, new_head):
+        self._head = new_head
+
+    @property
+    def tail(self):
+        return self._tail
+
+    @tail.setter
+    def tail(self, new_tail):
+        self._tail = new_tail
 
     @property
     def length(self):
@@ -79,20 +102,11 @@ class Edge(object):
     def length(self, new_len):
         self._length = new_len
 
-    def append(self, node):
-        assert len(self._nodes) < 2, 'Cannot add any more nodes to {}.'.format(self)
-        
-        self._nodes.append(node)
-        
-    def remove(self, node):
-
-        try:
-            self._nodes.remove(node)
-        except ValueError:
-            print('The node {} is not in edge {}.'.format(node, self))
+    @property
+    def name(self):
+        return 'Edge_'+ self.head.name + '_' + self.tail.name
 
     
-
 def read_fasta(file):
     """
     Function to read in a FASTA file and return a list of
@@ -116,7 +130,9 @@ def read_fasta(file):
 
                 line = f.readline()
     
-    return seqs
+    keys = [seq.name for seq in seqs]
+
+    return dict(zip(keys,seqs))
 
 def calc_dist(seq1, seq2):
     """
@@ -152,66 +168,88 @@ def build_matrix(seqlist):
         for m in range(n+1,l):
             dist_matrix[n,m]=dist_matrix[m,n]=calc_dist(seqlist[n].seq,seqlist[m].seq)
 
-    return labels, dist_matrix
+    return pd.DataFrame(columns=labels, data=dist_matrix, index=labels)
 
 def calc_qmat(dist_matrix):
     """
     Calculate the Q-matrix from the distance matrix and return the Q-matrix
     """
 
-    qmat = np.zeros_like(dist_matrix)
+    qmat = pd.DataFrame(data=np.zeros(dist_matrix.shape),
+            index=dist_matrix.index, columns=dist_matrix.columns)
 
-    (nrow, ncol) = np.shape(dist_matrix)
-    assert(nrow==ncol)
+    (nrow, ncol) = qmat.shape
+    assert nrow==ncol,'Matrix is not square'
 
     # The distance matrix is symmetric. Therefore the sum of all elements in
     # row i equals the sum of all elements in row j iff i==j
-    delta_sum = np.sum(dist_matrix, axis=0)
-
+    delta_sum = dist_matrix.sum(axis=0)
+    
     # Using the row/column sums that were already calculated will save computation
-    for i in range(nrow):
-        for j in range(ncol):
-            qmat[i,j] = (nrow-2)*dist_matrix[i,j] - delta_sum[i] - delta_sum[j]
+    for i in qmat.columns:
+        for j in qmat.index:
+            if i != j:
+                qmat.loc[i,j] = (nrow-2)*dist_matrix.loc[i,j] - delta_sum.loc[i] - delta_sum.loc[j]
 
     return qmat
     
 
 def build_tree(fastafile):
 
-    seqlist = read_fasta(fastafile)
-    nodeslist = []
-    edgelist = []
+    nodeslist = read_fasta(fastafile)
+    edgelist = {}
 
-    labels, dist_matrix = build_matrix(seqlist)
+    dist_matrix = build_matrix(nodeslist.values())
 
-    while len(labels)>1:
+    count = len(dist_matrix)
+    nodeNum = 1
+
+    while count>1:
 
         """ Calculate (joining) the Q-matrix """
         qmatrix = calc_qmat(dist_matrix)
 
         """ Branch length calculation """
-        indx1,indx2 = np.min(qmatrix)
-        
-        deltaindx1 = (dist_matrix[indx1,indx2]/2 + 1/(2*(len(labels)-2)) *
-                    (np.sum(dist_matrix[indx1,:]) - np.sum(dist_matrix[indx2,:])))
+        indx1,indx2 = np.unravel_index(qmatrix.values.argmin(), qmatrix.shape)
+        lbl1 = qmatrix.columns[indx1]
+        node1 = nodeslist[lbl1]
+        lbl2 = qmatrix.columns[indx2]
+        node2 = nodeslist[lbl2]
 
-        deltaindx2 = dist_matrix[indx1,indx2] - deltaindx1
-        
-        if deltaindx1 < 0 or deltaindx2 < 0:
-            deltaindx1 = (dist_matrix[indx2,indx1]/2 + 1/(2*(len(labels)-2)) *
-                        (np.sum(dist_matrix[indx2,:]) - np.sum(dist_matrix[indx1,:])))
+        deltaindx1 = (dist_matrix.iloc[indx1,indx2]/2 + 1/(2*(count-2)) *
+                    (np.sum(dist_matrix.iloc[indx1,:]) - np.sum(dist_matrix.iloc[indx2,:])))
 
-            deltaindx2 = dist_matrix[indx2,indx1] - deltaindx1
-    
+        deltaindx2 = dist_matrix.iloc[indx1,indx2] - deltaindx1
+        
         assert deltaindx1>=0 and deltaindx2>=0, 'An edge length is less than zero'
 
-        ## Create and append edges and nodes
+        # Create and append edges and nodes
+        newNode = 'Node'+nodeNum
+        nodeslist[newNode] = Node(name=newNode)
+        newEdge1 = Edge(lbl1, newNode, length=deltaindx1)
+        edgelist[newEdge1.name] = newEdge1
+        newEdge2 = Edge(lbl2, newNode, length=deltaindx2)
+        edgelist[newEdge2.name] = newEdge2
 
         """ Update distance matrix """
-        old_dist_matrix = dist_matrix
+        new_node_frame = 1/2 * (dist_matrix[lbl1]+
+                                dist_matrix[lbl2]-
+                                dist_matrix[lbl1][lbl2])
+
+        new_node_frame.name = newNode
+        new_node_frame[newNode] = 0
+        new_dist_matrix = dist_matrix.append(new_node_frame)
+        new_dist_matrix[newNode] = new_node_frame
+        
+        new_dist_matrix.drop([lbl1, lbl2], axis=0, inplace=True)
+        new_dist_matrix.drop([lbl1, lbl2], axis=1, inplace=True)
 
         """
         My thought here is that I will drop the indx1, indx2 columns and rows. Then
         append the new node in row 0 and column 0. Then recalculate.
         """
 
+        count -= 1
+        nodeNum += 1
+
+    return nodeslist, edgelist
